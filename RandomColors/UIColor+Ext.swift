d2fd1ff93823e9e622a3f2c443f1ef2930dc8da4
Bruno Albino <bruno-albino@hotmail.com>
2023-05-31 16:25:52 -0300

    create UIColor extension

--- RandomColors/UIColor+Ext.swift
+++ RandomColors/UIColor+Ext.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(
+            red: CGFloat.random(in: 0...1.0),
+            green: CGFloat.random(in: 0...1.0),
+            blue: CGFloat.random(in: 0...1.0),
+            alpha: 1
+        )
+    }
+}
